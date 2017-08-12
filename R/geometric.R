0edc0aebdb2c952159785ced2ac2c83f96b0fe12
Romain Francois <romain@thinkr.fr>
2017-08-12 11:01:49 +0200

    diamond( "small", "orange") -> 🔸
    closes #20

--- R/geometric.R
+++ R/geometric.R
@@ -1,13 +1,25 @@
 
-#' square emoji
+#' geometric emoji
 #'
-#' @param size size of the square ("small", "medium", "medium-small" or "large")
-#' @param color "white" or "black"
+#' @rdname geometric
+#' @param size size
+#' @param color color
+#'
+#' @details
+#' For `square`:
+#' - `size` should be one of `c( "small", "medium", "medium-small", "large" )`
+#' - `color` should be "white" or "black"
+#'
+#' For `diamond`
+#' - `size` should be "large" or "small"
+#' - `color` should be "orange" or "blue"
 #'
 #' @examples
 #' \dontrun{
 #' square( "small", "black" )
 #' square( "large", "white" )
+#'
+#' diamond( "small", "orange")
 #' }
 #'
 #' @importFrom dplyr filter
@@ -33,5 +45,28 @@ square <- function(size = c( "small", "medium", "medium-small", "large" ), color
 
 #' @export
 print.square <- function(x, ...) {
+  NextMethod()
+}
+
+#' @rdname geometric
+#' @export
+diamond <- function( size = c("large", "small"), color = c("orange", "blue") ){
+  size <- match.arg(size)
+  color <- match.arg(color)
+
+  data <- emo::jis %>%
+    filter( name == glue("{size} {color} diamond") )
+
+  structure(
+    data$emoji,
+    class = c("diamond", "geometric", "emoji"),
+    size = size,
+    color = color
+  )
+
+}
+
+#' @export
+print.diamond <- function(x, ...) {
   NextMethod()
 }
