4ae1b5f3baa14c6ac5f16b4aacb210d1301e185e
Romain Francois <romain@thinkr.fr>
2017-08-14 22:48:15 +0200

    merge tags (from gemoji) with keywords (from unicode data). #21

--- data-raw/jis.R
+++ data-raw/jis.R
@@ -67,5 +67,34 @@ parse_emoji_list <- function(
   jis
 }
 
+
+# read data from github/gemoji repo
+parse_gemoji <- function(){
+  data <- read_json("data-raw/gemoji/db/emoji.json")
+  keep <- data %>% map( "emoji" ) %>% map_lgl(negate(is.null))
+  data <- data[keep]
+
+  gemoji <- tibble(
+    emoji = map_chr(data, "emoji"),
+    aliases = map(data, "aliases") %>% map( . %>% flatten_chr()),
+    tags = map(data, "tags") %>% map( . %>% flatten_chr()),
+    unicode_version = map_chr(data, "unicode_version"),
+    ios_version = map_chr(data, "ios_version")
+  ) %>%
+    mutate( unicode_version = ifelse(unicode_version=="", NA, unicode_version) ) %>%
+    mutate_at( vars(ends_with("version")), as.numeric )
+
+  gemoji
+}
+
 jis <- parse_emoji_list()
+gemoji <- parse_gemoji()
+
+jis <- left_join( jis, gemoji, by = "emoji" )
+
+# merge tags and keywords
+jis <- jis %>%
+  mutate( keywords = map2(keywords, tags, ~unique( c(.x, .y))) ) %>%
+  select( -tags )
+
 use_data( jis, overwrite = TRUE)
