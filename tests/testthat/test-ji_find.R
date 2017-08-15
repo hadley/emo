ad9ed7cbf7d4acdc9e8326a33fef304e7ca072f2
Romain Francois <romain@thinkr.fr>
2017-08-15 19:08:27 +0200

    use :ant:  in `ji_find` test

--- tests/testthat/test-ji_find.R
+++ tests/testthat/test-ji_find.R
@@ -1,6 +1,6 @@
 context("ji_find")
 
 test_that("can find names", {
-  out <- ji_find("beer")
-  expect_equal(out$emoji, as.character(ji("beer")))
+  out <- ji_find("ant")
+  expect_equal(out$emoji, as.character(ji("ant")))
 })
