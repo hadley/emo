02e138cfefeffea77cc911951cc7088c25b00363
Romain Francois <romain@thinkr.fr>
2017-08-15 14:30:58 +0200

    simplify `ji_glue`

--- R/ji_glue.R
+++ R/ji_glue.R
@@ -1,63 +1,5 @@
-#' @importFrom stringr str_sub<- str_replace_all str_extract_all str_locate_all
-#' @importFrom purrr map_chr map2_chr
+
 ji_glue_one <- function(txt){
-  rx <- ":[^:]*:[*]?"
-  pos <- str_locate_all(txt, rx)[[1]]
-
-  chunks <- str_extract_all(txt, rx)[[1]]
-  list   <- str_detect( chunks, "[*]$")
-  chunks <- str_replace_all( chunks,  ":[*]?", "")
-
-  emojis <- map2_chr(
-    chunks, list, ~{
-      if(identical(.x, "")){
-        "::"
-      } else {
-        tryCatch({
-          if(.y){
-            paste( eval( parse( text = paste0( "ji_set(", .x, ")" ) ) ), collapse = " " )
-          } else {
-            eval( parse( text = paste0( "jitsu(", .x, ")" ) ) )
-          }
-        }, error = function(e) .x)
-      }
-    }
-  )
-
-  for( i in rev(seq_along(emojis)) ){
-    str_sub(txt, pos[i,1], pos[i,2]) <- emojis[i]
-  }
-
-  txt
-}
-
-#' emoji replacement
-#'
-#' @param txt character vector in which to replace `:something:` by an
-#' emoji for `something`, or `:something:*` by the collapsed
-#' list of all emojis relevant to `something`.
-#'
-#' @details
-#' - For the single form `:something:` [jitsu] is used on each pattern wrapped in `:`
-#' - For the list form `:something:*` [ji_set] is used
-#'
-#' @examples
-#' \dontrun{
-#'   ji_glue( "My :cat,face,love: loves to :sleep,zzz:" )
-#'
-#'   ji_glue( ":monkey:* loves to :dance:" )
-#'
-#'   ji_glue( ":technologist:* do :cool: stuff")
-#' }
-#' @export
-ji_glue <- function(txt){
-  structure(
-    map_chr(txt, ji_glue_one),
-    class = "emoji"
-  )
-}
-
-ji_replace_one <- function(txt){
   rx <- ":([a-zA-Z0-9_]*):"
   pos <- str_locate_all(txt, rx)[[1]]
 
@@ -78,10 +20,23 @@ ji_replace_one <- function(txt){
 }
 
 
+#' emoji glue
+#'
+#' @param txt character vector in which to replace `:something:` by an
+#' emoji for `something`. see [ji()] for what `something` can be.
+#'
+#' This is insipred from [glue::glue()] but uses `:` instead of braces
+#' because that's typically how emojis are
+#' [expressed](https://www.webpagefx.com/tools/emoji-cheat-sheet/)
+#'
+#' @examples
+#' \dontrun{
+#'   ji_glue( ":cat:s love to :zzz:" )
+#' }
 #' @export
-ji_replace <- function(txt){
+ji_glue <- function(txt){
   structure(
-    map_chr(txt, ji_replace_one),
+    map_chr(txt, ji_glue_one),
     class = "emoji"
   )
 }
