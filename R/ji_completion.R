451af32e8394463343f869362975ca7b9d49226c
Romain Francois <romain@thinkr.fr>
2017-08-17 21:19:43 +0200

    include the alias in the completion

--- R/ji_completion.R
+++ R/ji_completion.R
@@ -8,8 +8,17 @@
 #' @importFrom stringr str_detect
 #' @export
 ji_completion <- function( token ){
+  matches <- emo::ji_name[ str_detect(names(emo::ji_name), token ) ]
+  matches <- matches[ !duplicated(matches) ]
   structure(
-    unique( emo::ji_name[ str_detect(names(emo::ji_name), token ) ] ),
-    class = c("completion", "emoji")
+    unique( paste0( matches, " :", names(matches), ":" ) ),
+    class = c("emoji_completion")
   )
+}
+
+#' @importFrom purrr walk
+#' @export
+print.emoji_completion <- function(x, ...){
+  walk(x, writeLines )
+  invisible(x)
 }
