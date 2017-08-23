1f0f047e2850cd88e3fc46df1cadddc6f932e979
Jim Hester <james.f.hester@gmail.com>
2017-08-23 11:11:32 -0400

    Register a emoji completions for :xyz: within quotes
    This works on the R console and in the IDE.

--- R/ji_completion.R
+++ R/ji_completion.R
@@ -22,3 +22,35 @@ print.emoji_completion <- function(x, ...){
   writeLines( paste(x, " : ", names(x)) )
   invisible(x)
 }
+
+emoji_completer <- function(env) {
+  if (completeme::inside_quotes(env) && grepl(":[^[:space:]]*:?$", env$token)) {
+    # The IDE does not tokenize the words like the console
+    token <- sub("[^:]*:", ":", env$token)
+
+    if (grepl("^:[^[:space:]]+:$", token)) {
+      emoji <- emo::ji(gsub(":", "", token))
+    } else {
+      res <- ji_completion(sub(":", "", token))
+
+      if (length(res) == 0) {
+        return(FALSE)
+      }
+
+      if (length(res) == 1) {
+        emoji <- emo::ji(names(res))
+      } else {
+        emoji <- paste0(res, " : ", names(res))
+      }
+    }
+    env$fileName <- emoji
+    env$comps <- emoji
+
+    return(TRUE)
+  }
+  return(FALSE)
+}
+
+.onLoad <- function(pkg, lib) {
+  completeme::register_completion(emo = emoji_completer)
+}
